f03882413675bf56d225ac9704c516a755f2f54b
Mothergoose31 <JosephAlvarengaBeech@protonmail.com>
2019-09-04 14:10:13 -0700

    working on css

--- src/Header.jsx
+++ src/Header.jsx
@@ -17,7 +17,7 @@ class Header extends Component {
             <ul id="nav" className="nav">
             <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
             <li><a className="smoothscroll" href="#about">About</a></li>
-	            <li><a className="smoothscroll" href="#resume">Resume</a></li>
+
             <li><a className="smoothscroll" href="#portfolio">portfolio</a></li>
             
             <li><a className="smoothscroll" href="#contact">Contact</a></li>
